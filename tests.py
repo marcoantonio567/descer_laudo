622fff8d770a0ba289b3930ed04b2611c4d73e43
marco <2mbetwin@gmail.com>
2025-04-01 15:25:19 -0300

    arrumando o fluid

---
+++ tests.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+def abrir_link(url):
+    webbrowser.open(url)
+
+# Substitua pelo link desejado
+link = "https://www.google.com"
+abrir_link(link)
+
+
---
+++ textos.py
@@ -0,0 +1,18 @@
+def nome_modelo(fluid,proponente):
+    return f'Processo Nº {fluid} {proponente}'
+
+caminho_rural = r"G:\\Meu Drive\\AVALIAÇÃO\\2. Rurais"
+caminho_urbano = r"G:\\Meu Drive\\AVALIAÇÃO\\3. Urbanos"
+caminho_maquinario = r"G:\\Meu Drive\\AVALIAÇÃO\\4. Maquinarios"
+
+estrutura_de_pastas_rural = [('DOCUMENTOS','PNG'),'ENVIADOS','FOTOS','MAPAS','PEÇAS TECNICAS','SHAPES']
+estrutura_de_pastas_urbano = [('DOCUMENTOS','PNG'),'ENVIADOS','FOTOS','PEÇAS TECNICAS']
+estrutura_de_pastas_maquinario = [('DOCUMENTOS','PNG'),'ENVIADOS','FOTOS','PEÇAS TECNICAS']
+
+link = "https://docs.google.com/spreadsheets/d/1_7uhS6uNLeqV1nEC8MhT-wqDaZxG5DExVXKbEzbKD60/edit?gid=185532987#gid=185532987"
+
+
+
+
+
+
